ecd69112a78402a233c722afaba98fa37ec2488e
mawed22 <foupouamohamed3@gmail.com>
2023-03-12 18:18:58 +0100

    Commit version 1.0

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'af4884b2camshacc47edc57e7506p19a737jsn2320e9551d29',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d88e914be21ae5c59ece01545316140f";
